a4d66cddb5feb3302d1d6aeaf2e66c8fb5d16e8f
warren <wpersonal04@gmail.com>
2026-03-19 19:24:56 -0500

    Added solutions to AVL tree section.

--- Assignment3/Homework3-sourcecode/bst.py
+++ Assignment3/Homework3-sourcecode/bst.py
@@ -24,29 +24,135 @@ class BST:
         self.clear(self.root)
         self.root = None
 
+
+    # def clear(self, node: Optional[BSTNode]): Release (deallocate) the memory of all nodes in the
+    # subtree whose root is given by the parameter node. In Python, this can be done by assigning each
+    # node reference to None.
     def clear(self, node: Optional[BSTNode]) -> None:
-        # YOUR CODE HERE
-        pass
+        # base case to end recursion at the end of the tree
+        if node is None:
+            return
 
+        # recursive clearing
+        self.clear(node.left)
+        self.clear(node.right)
+        
+        # remove values
+        node.left = None
+        node.right = None
+
+
+    # def find(self, key: T, node: Optional[BSTNode]): Find the given key in the subtree whose
+    # root is the parameter node. If the key exists in the subtree, return a pointer to the node containing
+    # the key; otherwise, return the NULL pointer.
     def _find(self, key: T, node: Optional[BSTNode]) -> Optional[BSTNode]:
-        # YOUR CODE HERE
+        # get the root
+        current = self.root
+
+        # now while this node is not null
+        while current is not None:
+            # if we found the key
+            if key == current.key:
+                return current
+            # key < this key move to left
+            elif key < current.key:
+                current = current.left
+            # key > this key move to right
+            else: 
+                current = current.right
+
         return None  # Placeholder
 
+
+    # def findMaximum(self, node: Optional[BSTNode]): Find the maximum key in the subtree whose
+    # root is the parameter node. Return a pointer to the node containing the maximum key. If the subtree
+    # is empty, return the NULL pointer. The findMinimum function is similar.
     def _findMaximum(self, node: Optional[BSTNode]) -> Optional[BSTNode]:
-        # YOUR CODE HERE
-        return None  # Placeholder
+        # if node is null stop
+        if node is None:
+            return None
+        
+        # for iteration
+        current = node
+
+        # while right node is not null
+        # iterratie the current node->right
+        while current.right is not None:
+            current = current.right
+
+        return current  
+
 
     def _findMinimum(self, node: Optional[BSTNode]) -> Optional[BSTNode]:
-        # YOUR CODE HERE
-        return None  # Placeholder
+        # if node is null stop
+        if node is None:
+            return None
+        
+        # for iteration
+        current = node
 
+        # while left node is not null
+        # iterratie the current node->left
+        while current.left is not None:
+            current = current.left
+
+        return current  
+
+
+    # def insert(self, key: T, node: Optional[BSTNode]): Insert the given key into the subtree
+    # whose root is the parameter node. Return the root pointer of the subtree after insertion. If the key
+    # already exists, return the root unchanged (duplicate keys are not inserted).
     def _insert(self, key: T, node: Optional[BSTNode]) -> Optional[BSTNode]:
-        # YOUR CODE HERE
-        return None  # Placeholder
+        # escape the recursion
+        # create new node at first empty spot
+        # (after correct traversal)
+        if node is None:
+            return BSTNode(key)
 
+        # if we have found a node with the same key
+        # that we are trying to insert
+        if key == node.key:
+            return node
+        
+        # now to recursively insert
+        if key < node.key:
+            node.left = self._insert(key, node.left)
+        else:
+            node.right = self._insert(key, node.right)
+
+        return node
+    
+
+    # def remove(self, key: T, node: Optional[BSTNode]): Remove the given key from the sub-
+    # tree whose root is the parameter node. Return the root pointer of the subtree after deletion.
     def _remove(self, key: T, node: Optional[BSTNode]) -> Optional[BSTNode]:
-        # YOUR CODE HERE
-        return None  # Placeholder
+        # if we have found a node with the same key
+        # that we are trying to insert
+        if node is None:
+            return node
+        
+        # now to recursively search
+        if key < node.key:
+            node.left = self._remove(key, node.left)
+        elif key > node.key:
+            node.right = self._remove(key, node.right)
+            
+        else:
+            # node with 0 or 1 children:
+
+            # no left child
+            if node.left is None:
+                return node.right
+            # no right child
+            if node.right is None:
+                return node.left
+            
+            # node with 2 children
+            temp = self._findMinimum(node.right)
+            node.key = temp.key
+            node.right = self._remove(temp.key, node.right)
+
+        return node
 
     def find(self, key: T) -> Optional[BSTNode]:
         return self._find(key, self.root)
